23f0eddb68720c923ddd68a1b0746ed1f9b7743b
Daniel Shi <181678706+Snazzbazz101@users.noreply.github.com>
2025-06-14 03:31:31 -0700

    Made text size larger, and my name a different color

--- website/components/hero.tsx
+++ website/components/hero.tsx
@@ -1,11 +1,12 @@
 import Picture from "./picture"
+import { TextGenerateEffect } from "./ui/text-generate-effect";
 
 export default function Hero() {
     return (
     <div className="flex mt-16 ml-20 items-center gap-25">
         <Picture/>
         <h1 className="text-8xl font-bold text-black">
-          Hello, I&apos;m Daniel...
+          <TextGenerateEffect words="Hi I'm Daniel..."/>
         </h1>
       </div>
     );
