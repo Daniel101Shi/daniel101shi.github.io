c313c61d2472a8c0f8c4b735736da76fb265a738
Daniel Shi <181678706+Snazzbazz101@users.noreply.github.com>
2025-06-15 14:17:59 -0700

    Added proper componenets, and make sure local deployment works.

--- website/next.config.ts
+++ website/next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/DANIEL101SHI.GITHUB.IO/' : '',
+  basePath: isProd ? '/DANIEL101SHI.GITHUB.IO' : '',
+  output: 'export'
 };
 
 export default nextConfig;
